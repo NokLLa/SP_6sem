66369aa67f10b05a3e4f17d9d4a9a102666c1363
nk <nokla133711@gmail.com>
2023-04-04 00:51:50 +0300

    10lab

---
+++ OS10_File_Mapping/TEST_CASES/tests.h
@@ -0,0 +1,13 @@
+#pragma once
+#pragma comment(lib, "C:\\Users\\nk\\Desktop\\OS10_File_Mapping\\x64\\Debug\\OS10_HTAPI.lib")
+
+#include "../OS10_HTAPI/pch.h"
+#include "../OS10_HTAPI/HT.h"
+
+namespace tests
+{
+	BOOL test1(ht::HtHandle* htHandle);
+	BOOL test2(ht::HtHandle* htHandle);
+	BOOL test3(ht::HtHandle* htHandle);
+	BOOL test4(ht::HtHandle* htHandle);
+}
